c944bff9217bc550e09b7c42f2333a4730c9e18d
Karthiks49 <karthik.subramani@ideas2it.com>
2021-12-24 21:15:26 +0530

    Completed right side layout

--- toDo.js
+++ toDo.js
@@ -1,3 +1,4 @@
+
 function hide() {
     document.getElementById("left-portion").style.display = 'none';
     document.getElementById("menu-icon-right").style.display = 'block';
@@ -6,17 +7,29 @@ function hide() {
 function show() {
     document.getElementById("left-portion").style.display = 'block';
     document.getElementById("menu-icon-right").style.display = 'none';
-    
 }
 
 function myDay() {
-    document.getElementById("right-portion").style.display = 'block';
+    document.getElementById("myday-add-task-container-additional").style.display = 'none';
+    document.getElementById("myday-add-task-container").style.display = 'flex';
+    document.getElementById("right-portion-myday").style.display = 'block';
 }
 
-    const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const months = ["January","February","March","April","May","June","July","August","September","October","Novermber","December"];
-    const date = new Date();
-    let day = weekdays[date.getDay()];
-    let month = months[date.getMonth()];
-    let dateToday = date.getDate();
-    document.getElementById("date-and-day").textContent = day;
+function important() {
+    document.getElementById("right-portion-myday").style.display = 'none';
+}
+
+function addTaskAdditional() {
+    document.getElementById("myday-add-task-container").style.display = 'none';
+    document.getElementById("myday-add-task-container-additional").style.display = 'block';
+}
+
+function dateDay() {
+var today = new Date();
+var day = today.getDay();
+var month = today.getMonth();
+var date = today.getDate();
+var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("date-and-day").innerText = daylist[day] + ', ' + monthList[month] + ' ' + date;
+}
