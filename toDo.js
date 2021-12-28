4b6614e2fe981234b3924738518b66c095c31f65
Karthiks49 <karthik.subramani@ideas2it.com>
2021-12-28 22:46:48 +0530

    Completed left side panel with JS

--- toDo.js
+++ toDo.js
@@ -6,29 +6,35 @@ var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday"
 var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("date-and-day").innerText = daylist[day] + ', ' + monthList[month] + ' ' + date;
 
-function hide() {
-    document.getElementById("left-portion").style.display = 'none';
-    document.getElementById("menu-icon-right").style.display = 'block';
+
+var items= ["My Day", "Important", "Planned", "Assigned to me", "Flagged email", "Task"];
+
+for(menuItem of items) {
+    let subElements = document.createElement("div");
+    let title = document.createElement("div");
+    let count = document.createElement("div");
+    subElements.setAttribute("class", "menu-item");
+    title.setAttribute("class", "left-option-heading");
+    title.innerHTML = menuItem;
+    count.setAttribute("class", "menu-count");
+    subElements.appendChild(title);
+    subElements.appendChild(count);
+    document.getElementsByClassName("left-options")[0].appendChild(subElements);
 }
 
-function show() {
-    document.getElementById("left-portion").style.display = 'block';
-    document.getElementById("menu-icon-right").style.display = 'none';
+document.getElementById("input-list").addEventListener("keydown", addList);
+
+function addList(event) {
+    if (event.keyCode === 13) {
+        var inputList = document.getElementById("input-list").value;
+        items.push(inputList);
+        let subElements = document.createElement("div");
+        let title = document.createElement("div");
+        title.setAttribute("class", "left-option-heading");
+        title.innerHTML = inputList;
+        subElements.appendChild(title);
+        document.getElementsByClassName("left-options")[0].appendChild(subElements);
+        document.getElementById("input-list").value = "";
+    }
 }
-
-function myDay() {
-    document.getElementById("myday-add-task-container-additional").style.display = 'none';
-    document.getElementById("myday-add-task-container").style.display = 'flex';
-    document.getElementById("right-portion-myday").style.display = 'block';
-}
-
-function important() {
-    document.getElementById("right-portion-myday").style.display = 'none';
-}
-
-function addTaskAdditional() {
-    document.getElementById("myday-add-task-container").style.display = 'none';
-    document.getElementById("myday-add-task-container-additional").style.display = 'block';
-}
-
-
+console.log(items);
